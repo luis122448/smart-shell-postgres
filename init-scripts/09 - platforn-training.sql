f0531724c0ab37d5d9da1448fae273408d38fe53
luis122448 <luis122448@gmail.com>
2024-01-18 20:17:36 -0500

    Add create database platform-training

--- init-scripts/09 - platforn-training.sql
+++ init-scripts/09 - platforn-training.sql
@@ -1,8 +1,3 @@
--- Configuración de parámetros
-ALTER SYSTEM SET shared_buffers = '512MB';
-ALTER SYSTEM SET work_mem = '64MB';
-ALTER SYSTEM SET effective_cache_size = '2GB';
-
 -- Nombre de la base de datos
 CREATE DATABASE platform_training;
 
@@ -10,10 +5,10 @@ CREATE DATABASE platform_training;
 \c platform_training;
 
 -- Crear un usuario con privilegios de superusuario (puedes personalizar según tus necesidades)
-CREATE USER nombre_de_usuario WITH PASSWORD 'tu_contraseña' SUPERUSER;
+CREATE USER developer WITH PASSWORD '1073741824' SUPERUSER;
 
 -- Habilitar las extensiones necesarias (puedes agregar otras según tus necesidades)
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Otorgar permisos al usuario sobre la base de datos
-GRANT ALL PRIVILEGES ON DATABASE platform_training TO nombre_de_usuario;
+GRANT ALL PRIVILEGES ON DATABASE platform_training TO developer;
