de55274bc23839078e82d8cfff49cc6675129948
luis122448 <luis122448@gmail.com>
2024-01-18 20:31:45 -0500

    Modify create database platform-training

--- init-scripts/09 - platforn-training.sql
+++ init-scripts/09 - platforn-training.sql
@@ -1,14 +1,14 @@
 -- Nombre de la base de datos
-CREATE DATABASE platform_training;
+-- CREATE DATABASE platform_training;
 
--- Cambia al contexto de la nueva base de datos
-\c platform_training;
+-- -- Cambia al contexto de la nueva base de datos
+-- \c platform_training;
 
--- Crear un usuario con privilegios de superusuario (puedes personalizar según tus necesidades)
-CREATE USER developer WITH PASSWORD '1073741824' SUPERUSER;
+-- -- Crear un usuario con privilegios de superusuario (puedes personalizar según tus necesidades)
+-- CREATE USER developer WITH PASSWORD '1073741824' SUPERUSER;
 
--- Habilitar las extensiones necesarias (puedes agregar otras según tus necesidades)
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- -- Habilitar las extensiones necesarias (puedes agregar otras según tus necesidades)
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Otorgar permisos al usuario sobre la base de datos
-GRANT ALL PRIVILEGES ON DATABASE platform_training TO developer;
+-- -- Otorgar permisos al usuario sobre la base de datos
+-- GRANT ALL PRIVILEGES ON DATABASE platform_training TO developer;
