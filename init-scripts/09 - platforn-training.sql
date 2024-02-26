5b46b78da3df57413f5c5f2551d96d7728ce8d17
luis122448 <luis122448@gmail.com>
2024-02-25 23:36:50 -0500

    Add new SP for cancel documento

--- init-scripts/09 - platforn-training.sql
+++ init-scripts/09 - platforn-training.sql
@@ -1,15 +1,15 @@
 -- Nombre de la base de datos
--- CREATE DATABASE platform_training;
+CREATE DATABASE platform_training;
 
 -- -- Cambia al contexto de la nueva base de datos
--- \c platform_training;
+\c platform_training;
 
 -- -- Crear un usuario con privilegios de superusuario (puedes personalizar según tus necesidades)
--- CREATE USER developer WITH PASSWORD '1073741824' SUPERUSER;
+CREATE USER developer WITH PASSWORD '1073741824' SUPERUSER;
 
 -- -- Habilitar las extensiones necesarias (puedes agregar otras según tus necesidades)
--- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- -- Otorgar permisos al usuario sobre la base de datos
--- GRANT ALL PRIVILEGES ON DATABASE platform_training TO developer;
+GRANT ALL PRIVILEGES ON DATABASE platform_training TO developer;
 
