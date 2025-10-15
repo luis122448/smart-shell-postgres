5335d5455db4c9ef187081639350884c022193d9
Luis Antonio Calvo Quispe <luis122448@gmail.com>
2025-10-14 23:18:13 -0500

    deploy: update volumn for docker-compose

--- install.sh
+++ install.sh
@@ -2,15 +2,3 @@
 
 # Create environment variables file
 touch .env
-
-# Delete directories
-rm -rf /var/www/volumes/smart-shell/smart-shell-postgres/data
-rm -rf /var/www/volumes/smart-shell/smart-shell-postgres/logs
-rm -rf /var/www/volumes/smart-shell/smart-shell-postgres/backups
-rm -rf /var/www/volumes/smart-shell/smart-shell-postgres/configurations
-
-# Create directories
-mkdir -p /var/www/volumes/smart-shell/smart-shell-postgres/data
-mkdir -p /var/www/volumes/smart-shell/smart-shell-postgres/logs
-mkdir -p /var/www/volumes/smart-shell/smart-shell-postgres/backups
-mkdir -p /var/www/volumes/smart-shell/smart-shell-postgres/configurations
